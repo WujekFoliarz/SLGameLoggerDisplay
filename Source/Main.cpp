d4dc69b3d83eb691f7922124ce6922a6e341b6ed
WujekFoliarz <72314465+WujekFoliarz@users.noreply.github.com>
2026-09-16 17:19:20 +0200

    remove unused includes from Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,17 +1,3 @@
-#include "raylib.h"
-
-#include <fstream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <print>
-#include <algorithm>
-
-#include <nlohmann/json.hpp>
-
-#include "Events.hpp"
-#include "Locations.hpp"
-#include "AnnounceLog.hpp"
 #include "ReplayPlayer.hpp"
 
 int main()
